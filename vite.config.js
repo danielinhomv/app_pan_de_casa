9865b60c184ea989c2aef69394e74384f830b21b
danielinho <montanovargasdaniel39@gmail.com>
2026-07-01 17:44:17 -0400

    modificacion del vite.config.js para que este listo y mantenible tanto para produccion y local

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.ASSET_URL ? env.ASSET_URL + '/build/' : '/build/',
+
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
+            }),
+        ],
+    };
 });
